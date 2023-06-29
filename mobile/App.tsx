672d186839f75b32606f18e254fc09ee0306a57c
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-06-29 09:00:24 -0300

    project-changed-to-styles-components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { lightTheme, darkTheme } from "./src/theme/theme";
+import Button from "./src/components/Button";
+import { Container, Layout, ScrollContainer } from "./src/components/Layout";
+import { ThemeProvider } from "styled-components/native";
+import { useThemeMode } from "./src/hooks/useThemeMode";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "SF-Pro-Display-Regular": require("./assets/fonts/SFPRODISPLAYREGULAR.otf"),
+    "SF-Pro-Display-Medium": require("./assets/fonts/SFPRODISPLAYMEDIUM.otf"),
+    "SF-Pro-Display-SemiBold": require("./assets/fonts/SF-Pro-Display-Semibold.ttf"),
+    "SF-Pro-Display-Bold": require("./assets/fonts/SFPRODISPLAYBOLD.otf"),
+  });
+  const { theme, toggleTheme } = useThemeMode();
+
+  if (!fontsLoaded) return <Text>Loading</Text>;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Layout>
+        <ScrollContainer>
+          <Container>
+            <Button onPress={toggleTheme}>OI</Button>
+          </Container>
+        </ScrollContainer>
+      </Layout>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
