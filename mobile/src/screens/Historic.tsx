0d23b17e47fb008716e655956710d848998d262e
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-07-01 18:38:38 -0300

    historic screen initialize

---
+++ mobile/src/screens/Historic.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Layout } from "../components/Layout";
+
+const Historic = () => {
+  return (
+    <Layout>
+      <Text>Historic</Text>
+    </Layout>
+  );
+};
+
+export default Historic;
