b16d39b27d9352052b5966b69dc9d09a84da128e
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-07-01 18:38:26 -0300

    favorites screen initialize

---
+++ mobile/src/screens/Favorites.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Layout } from "../components/Layout";
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <Text>Favorites</Text>
+    </Layout>
+  );
+};
+
+export default Favorites;
