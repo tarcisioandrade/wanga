5b0855631b8b5a42b15450cacea6e7421ebde3a0
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-07-01 18:36:54 -0300

    changed to inter font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,20 +2,34 @@ import "react-native-gesture-handler";
 import { lightTheme, darkTheme } from "./src/theme/theme";
 import { ThemeProvider } from "styled-components/native";
 import { useThemeMode } from "./src/hooks/useThemeMode";
-import { useFonts } from "expo-font";
 import { Text } from "react-native";
 import Routes from "./src/routes/Navigator";
 import { StatusBar } from "expo-status-bar";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useAppStateChange } from "./src/hooks/useAppStateChange";
-
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "SF-Pro-Display-Regular": require("./assets/fonts/SFPRODISPLAYREGULAR.otf"),
-    "SF-Pro-Display-Medium": require("./assets/fonts/SFPRODISPLAYMEDIUM.otf"),
-    "SF-Pro-Display-SemiBold": require("./assets/fonts/SF-Pro-Display-Semibold.ttf"),
-    "SF-Pro-Display-Bold": require("./assets/fonts/SFPRODISPLAYBOLD.otf"),
+  // const [fontsLoaded] = useFonts({
+  //   "SF-Pro-Display-Regular": require("./assets/fonts/SFPRODISPLAYREGULAR.otf"),
+  //   "SF-Pro-Display-Medium": require("./assets/fonts/SFPRODISPLAYMEDIUM.otf"),
+  //   "SF-Pro-Display-SemiBold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+  //   "SF-Pro-Display-Bold": require("./assets/fonts/SFPRODISPLAYBOLD.otf"),
+  // });
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
   });
+
   const { theme } = useThemeMode();
   const statusStyle = theme === "dark" ? "light" : "dark";
 
