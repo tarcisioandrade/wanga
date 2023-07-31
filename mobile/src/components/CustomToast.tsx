a5c2d1b75a4aed3ede1627d6ead11f34e0925004
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-07-31 10:29:57 -0300

    improvement in custom toast

--- mobile/src/components/CustomToast.tsx
+++ mobile/src/components/CustomToast.tsx
@@ -3,22 +3,41 @@ import { BaseToast, BaseToastProps } from "react-native-toast-message";
 import { ms } from "src/utils/metrics";
 import { useTheme } from "styled-components";
 
-const CustomToast = (props: BaseToastProps) => {
+type CustomToastProps = BaseToastProps & {
+  type?: "error" | "success" | "info";
+};
+
+const CustomToast = ({ type, ...props }: CustomToastProps) => {
   const theme = useTheme();
+
+  const borderColors = {
+    error: theme.RED_500,
+    success: theme.GREEN_500,
+    info: theme.PRIMARY,
+  };
 
   return (
     <BaseToast
       {...props}
-      style={{ backgroundColor: theme.SECONDARY, height: 50 }}
+      style={{
+        backgroundColor: theme.SECONDARY,
+        borderLeftColor: type && borderColors[type],
+      }}
       text1Style={{
         color: theme.TEXT_COLOR,
         fontWeight: "bold",
         fontSize: ms(15),
       }}
+      text2Style={{
+        fontSize: ms(11),
+      }}
+      text2NumberOfLines={2}
     />
   );
 };
 
 export const toastConfig = {
-  success: (props: BaseToastProps) => <CustomToast {...props} />,
+  success: (props: BaseToastProps) => <CustomToast {...props} type="success" />,
+  error: (props: BaseToastProps) => <CustomToast {...props} type="error" />,
+  info: (props: BaseToastProps) => <CustomToast {...props} type="info" />,
 };
