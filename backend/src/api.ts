9a033445e3610f489fc9a7ed16b5704f53515dfd
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-08-05 16:14:30 -0300

    user agent in header

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -250,10 +250,14 @@ export function getTop(page: string) {
         "https://mangalivre.net/series/index/nota?page=" + page,
         {
           http2: true,
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+          },
         }
       );
 
-      console.log("response", response);
+      console.log("response", response.request);
       return_data.data = parseResults(response.body);
     } catch (error) {
       if (error instanceof Error) {
