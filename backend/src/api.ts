90f7c513e44128b2565487a2aae31454df23b08a
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-08-07 11:50:40 -0300

    https agent

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -257,6 +257,7 @@ export function getTop(page: string) {
         {
           agent: {
             http: agent,
+            https: agent,
           },
           https: {
             rejectUnauthorized: false,
